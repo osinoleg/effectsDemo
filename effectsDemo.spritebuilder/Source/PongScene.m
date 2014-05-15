11f9b39e66a49206e3dcd6fe846866190d1d2e83
Oleg Osin <oleg@apportable.com>
2014-05-14 19:16:26 -0700

    Set up skeleton pong game.

--- effectsDemo.spritebuilder/Source/PongScene.m
+++ effectsDemo.spritebuilder/Source/PongScene.m
@@ -10,12 +10,94 @@
 #import "CCDirector.h"
 #import  "CCEffectNode.h"
 
-@implementation PongScene
+#define PADDLE_HEIGHT 100.0f
+#define PADDLE_WIDTH 30.0f
 
+#define BALL_HEIGHT 10.0f
+#define BALL_WIDTH 10.0f
+
+
+@implementation PongScene {
+    CCNodeColor* _leftPaddle;
+    CCNodeColor* _rightPaddle;
+    CCNodeColor* _ball;
+    CGSize _designSize;
+}
+
+- (float)centerPaddleY:(float)y
+{
+    float centerY = y - PADDLE_HEIGHT * 0.5f;
+    return centerY;
+}
 
 - (void) didLoadFromCCB
 {
-    NSLog(@"HELLO");
+    self.userInteractionEnabled = YES;
+
+    _designSize = [[CCDirector sharedDirector] designSize];
+    
+    _leftPaddle = [CCNodeColor nodeWithColor:[CCColor redColor]];
+    _leftPaddle.anchorPoint = ccp(0.0, 0.0);
+    _leftPaddle.contentSize = CGSizeMake(PADDLE_WIDTH, PADDLE_HEIGHT);
+    _leftPaddle.position = ccp(0.0, [self centerPaddleY:_designSize.height * 0.5f]);
+    [self addChild:_leftPaddle];
+    
+    _rightPaddle = [CCNodeColor nodeWithColor:[CCColor redColor]];
+    _rightPaddle.anchorPoint = ccp(0.0, 0.0);
+    _rightPaddle.contentSize = CGSizeMake(PADDLE_WIDTH, PADDLE_HEIGHT);
+    _rightPaddle.position = ccp(_designSize.width - PADDLE_WIDTH, [self centerPaddleY:_designSize.height * 0.5f]);
+    [self addChild:_rightPaddle];
+
+    // TODO: use physics body for cool collision effects, also we want to test cceffects with phyisics
+    _ball = [CCNodeColor nodeWithColor:[CCColor greenColor]];
+    _ball.anchorPoint = ccp(0.5, 0.5);
+    _ball.contentSize = CGSizeMake(BALL_WIDTH, BALL_HEIGHT);
+    _ball.position = ccp(_designSize.width * 0.5f, _designSize.height * 0.5f);
+    [self addChild:_ball];
+    
+    [self schedule:@selector(sceneUpdate:) interval:1.0f/60.0f];
+}
+
+- (void)sceneUpdate:(CCTime)interval
+{
+    
+}
+
+#pragma mark touch
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    
+    // pseudo
+    /*
+     CGPoint location = [touch locationInNode:node];
+     CGRect box = node.sprite.boundingBox;
+     
+     if(CGRectContainsPoint(box, location))
+     {
+     
+     }
+     */
+    
+    CGPoint location = [touch locationInNode:self];
+    if(location.x < _designSize.width * 0.5f)
+    {
+        _leftPaddle.position = ccp(_leftPaddle.position.x, [self centerPaddleY:location.y]);
+    }
+}
+
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint location = [touch locationInNode:self];
+    if(location.x < _designSize.width * 0.5f)
+    {
+        _leftPaddle.position = ccp(_leftPaddle.position.x, [self centerPaddleY:location.y]);
+    }
+}
+
+- (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    
 }
 
 @end
